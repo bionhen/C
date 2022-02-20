5acae9b78e8300af41f68343d2a4e4e13f2ccb42
Bionhen <liubimov.is@phystech.edu>
2022-02-20 10:52:29 +0300

    Объединенная программа

--- Laba/one_and_two_dim.cpp
+++ Laba/one_and_two_dim.cpp
@@ -9,110 +9,151 @@ int random(int div) {
     return a;
 }
 
-int creation(int area[], int n, int a) {
+int creation(int area[], int n, int a, int sign) {
     int count = 0;
-    while(count < n) {
-        int b = random(a * a);
-        if (area[b] == 0) {
-            area[b] = 1;
-            count++;
-        }
-    }
-    return 0;
-}
-
-int move(int area[], int a) {
-    for (int i = 0; i < a * a; i++) {
-        if (area[i] == 1) {
-            if (i % a == 0 || i < a || i > a * a - a - 1 || i % a == a - 1)
-                area[i] = 3;
-        }
-    }
-    for (int i = 0; i < a * a; i++) {
-        if (area[i] == 1) {
-            int step = random(4);
-            if (step == 0 && area[i + 1] == 0) {
-                area[i + 1] = 2;
-                area[i] = 0;
-            }
-            if (step == 1 && area[i + a] == 0) {
-                area[i + a] = 2;
-                area[i] = 0;
-            }
-            if (step == 2 && area[i - a] == 0) {
-                area[i - a] = 2;
-                area[i] = 0;
-            }
-            if (step == 3 && area[i - 1] == 0) {
-                area[i - 1] = 2;
-                area[i] = 0;
+    if (sign % 5 < 3) {
+        while (count < n) {
+            int b = random(a * a);
+            if (area[b] == 0) {
+                area[b] = 1;
+                count++;
             }
         }
     }
-    for (int i = 0; i < a * a; i++) {
-        if (area[i] == 2) {
-            if (i % a == 0 || i < a || i > a * a - a - 1 || i % a == a - 1)
-                area[i] = 3;
-            else { area[i] = 1; }
-        }
-    }
-    for (int i = 0; i < a * a; i++) {
-        if (area[i] == 1) {
-            if (area[i + 1] != 0 || area[i - 1] != 0 || area[i + a] != 0 || area[i - a] != 0) {
-                area[i] = 3;
+    else{
+        while (count < n) {
+            int b = random(a);
+            if (area[b] == 0) {
+                area[b] = 1;
+                count++;
             }
         }
     }
     return 0;
 }
 
-int count(int area[], int a) {
+int move(int area[], int a, int sign) {
+    if (sign%5<3) {
+        for (int i = 0; i < a * a; i++) {
+            if (area[i] == 1) {
+                if (i % a == 0 || i < a || i > a * a - a - 1 || i % a == a - 1)
+                    area[i] = 3;
+            }
+        }
+        for (int i = 0; i < a * a; i++) {
+            if (area[i] == 1) {
+                int step = random(4);
+                if (step == 0 && area[i + 1] == 0) {
+                    area[i + 1] = 2;
+                    area[i] = 0;
+                }
+                if (step == 1 && area[i + a] == 0) {
+                    area[i + a] = 2;
+                    area[i] = 0;
+                }
+                if (step == 2 && area[i - a] == 0) {
+                    area[i - a] = 2;
+                    area[i] = 0;
+                }
+                if (step == 3 && area[i - 1] == 0) {
+                    area[i - 1] = 2;
+                    area[i] = 0;
+                }
+            }
+        }
+        for (int i = 0; i < a * a; i++) {
+            if (area[i] == 2) {
+                if (i % a == 0 || i < a || i > a * a - a - 1 || i % a == a - 1)
+                    area[i] = 3;
+                else { area[i] = 1; }
+            }
+        }
+        for (int i = 0; i < a * a; i++) {
+            if (area[i] == 1) {
+                if (area[i + 1] != 0 || area[i - 1] != 0 || area[i + a] != 0 || area[i - a] != 0) {
+                    area[i] = 3;
+                }
+            }
+        }
+        return 0;
+    }
+    else {
+        for (int i = 0; i < a; i++) {
+            if (area[i] == 1) {
+                if (i == 0 || i == a - 1)
+                    area[i] = 3;
+            }
+        }
+        for (int i = 0; i < a; i++) {
+            if (area[i] == 1) {
+                int step = random(4);
+                if (step == 0 && area[i + 1] == 0) {
+                    area[i + 1] = 2;
+                    area[i] = 0;
+                }
+                if (step == 3 && area[i - 1] == 0) {
+                    area[i - 1] = 2;
+                    area[i] = 0;
+                }
+            }
+        }
+        for (int i = 0; i < a; i++) {
+            if (area[i] == 2) {
+                if (i == 0 || i == a - 1)
+                    area[i] = 3;
+                else { area[i] = 1; }
+            }
+        }
+        for (int i = 0; i < a; i++) {
+            if (area[i] == 1) {
+                if (area[i + 1] != 0 || area[i - 1] != 0) {
+                    area[i] = 3;
+                }
+            }
+        }
+        return 0;
+    }
+}
+
+int count(int area[], int a, int sign) {
     int c = 0;
-    for (int i = 0; i < a * a; i++) {
-        if (area[i] == 1) {
-            c++;
+    if (sign % 5 < 3) {
+        for (int i = 0; i < a * a; i++) {
+            if (area[i] == 1) {
+                c++;
+            }
+        }
+    }
+    else {
+        for (int i = 0; i < a; i++) {
+            if (area[i] == 1) {
+                c++;
+            }
         }
     }
     return c;
 }
 
-int process(int area[], int a) {
+int process(int area[], int a, int sign) {
     int i = 0;
-    int c = count(area, a);
+    int c = count(area, a, sign);
     while (c != 0) {
-        move(area, a);
-        c = count(area, a);
+        move(area, a, sign);
+        c = count(area, a, sign);
         i++;
     }
     return i;
 }
-int one_a_vs_time() {
+int one_a_vs_time(int sign) {
     int a = 0, c = 0, i = 0, sum = 0;
     float mean = 0;
     for (a = 2; a < 60; a++) {
         sum = 0;
-        for (int j = 0; j < 50000; j++) {
-            int area[3600] = { 0 };
-            creation(area, 1, a);
-            i = process(area, a);
-            sum += i;
-        }
-        mean = sum / 50000.0;
-        cout << mean << " " << '\n';
-    }
-    return 0;
-}
-
-int two_alpha_dislocation_vs_time() {
-    int c = 0, n = 0, sum = 0, time = 0;
-    float mean = 0;
-    for (n = 1; n < 400; n++) {
-        sum = 0;
         for (int j = 0; j < 10000; j++) {
             int area[3600] = { 0 };
-            creation(area, n, 20);
-            time = process(area, 20);
-            sum += time;
+            creation(area, 1, a, sign);
+            i = process(area, a, sign);
+            sum += i;
         }
         mean = sum / 10000.0;
         cout << mean << " " << '\n';
@@ -120,101 +161,15 @@ int two_alpha_dislocation_vs_time() {
     return 0;
 }
 
-int creation_one_dim(int area[], int n, int a) {
-    int count = 0;
-    while (count < n) {
-        int b = random(a);
-        if (area[b] == 0) {
-            area[b] = 1;
-            count++;
-        }
-    }
-    return 0;
-}
-
-int move_one_dim(int area[], int a) {
-    for (int i = 0; i < a * a; i++) {
-        if (area[i] == 1) {
-            if (i == 0 || i == a - 1)
-                area[i] = 3;
-        }
-    }
-    for (int i = 0; i < a; i++) {
-        if (area[i] == 1) {
-            int step = random(4);
-            if (step == 0 && area[i + 1] == 0) {
-                area[i + 1] = 2;
-                area[i] = 0;
-            }
-            if (step == 3 && area[i - 1] == 0) {
-                area[i - 1] = 2;
-                area[i] = 0;
-            }
-        }
-    }
-    for (int i = 0; i < a; i++) {
-        if (area[i] == 2) {
-            if (i == 0 || i == a - 1)
-                area[i] = 3;
-            else { area[i] = 1; }
-        }
-    }
-    for (int i = 0; i < a; i++) {
-        if (area[i] == 1) {
-            if (area[i + 1] != 0 || area[i - 1] != 0) {
-                area[i] = 3;
-            }
-        }
-    }
-    return 0;
-}
-
-int count_one_dim(int area[], int a) {
-    int c = 0;
-    for (int i = 0; i < a; i++) {
-        if (area[i] == 1) {
-            c++;
-        }
-    }
-    return c;
-}
-
-int process_one_dim(int area[], int a) {
-    int i = 0;
-    int c = count(area, a);
-    while (c != 0) {
-        move_one_dim(area, a);
-        c = count_one_dim(area, a);
-        i++;
-    }
-    return i;
-}
-int one_a_vs_time_one_dim() {
-    int a = 0, c = 0, i = 0, sum = 0;
-    float mean = 0;
-    for (a = 2; a < 200; a++) {
-        sum = 0;
-        for (int j = 0; j < 50000; j++) {
-            int area[3600] = { 0 };
-            creation_one_dim(area, 1, a);
-            i = process_one_dim(area, a);
-            sum += i;
-        }
-        mean = sum / 50000.0;
-        cout << mean << " " << '\n';
-    }
-    return 0;
-}
-
-int two_alpha_dislocation_vs_time_one_dim() {
+int two_alpha_dislocation_vs_time(int sign) {
     int c = 0, n = 0, sum = 0, time = 0;
     float mean = 0;
-    for (n = 1; n < 50; n++) {
+    for (n = 1; n < 400; n++) {
         sum = 0;
         for (int j = 0; j < 10000; j++) {
             int area[3600] = { 0 };
-            creation_one_dim(area, n, 50);
-            time = process_one_dim(area, 50);
+            creation(area, n, 20, sign);
+            time = process(area, 20, sign);
             sum += time;
         }
         mean = sum / 10000.0;
@@ -228,16 +183,16 @@ int main() {
     srand(static_cast<unsigned int>(time(0)));
     cin >> number;
     if (number == 1) {
-        one_a_vs_time();
+        one_a_vs_time(number);
     }
     if (number == 2) {
-        two_alpha_dislocation_vs_time();
+        two_alpha_dislocation_vs_time(number);
     }
     if (number == 3) {
-        one_a_vs_time_one_dim();
+        one_a_vs_time(number);
     }
     if (number == 4) {
-        two_alpha_dislocation_vs_time_one_dim();
+        two_alpha_dislocation_vs_time(number);
     }
     return 0;
 }
