b9f2f980eec38443e70c308597e1ba1bab93c8bf
Bionhen <liubimov.is@phystech.edu>
2022-03-03 21:58:40 +0300

    1-6

--- cmake-build-debug/2.cpp
+++ cmake-build-debug/2.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 
 int func() {
-    int a, n = 1;
+    int a, n = 0;
     cin >> a;
     n++;
     if (a == 0) {
         return 0;
     }
-    if (n == 1) {
-        cout <<'\n'<< a;
+    if (n % 2 == 1) {
+        cout << '\n' << a;
     }
     func();
+    return 0;
 }
 
 int main() {
