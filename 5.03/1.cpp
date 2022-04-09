3a36901dc58d591e2cd54e990b73322494731213
Bionhen <liubimov.is@phystech.edu>
2022-04-09 13:36:04 +0300

    1

---
+++ 5.03/1.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+
+using namespace std;
+
+struct Element {
+    int key;
+    Element * next;
+};
+
+void data_in(Element* &List) {
+    Element* tmp = new Element;
+    int a = 0;
+    cin >> a;
+    tmp->key = a;
+    tmp->next = new Element;
+    List = tmp;
+    tmp = tmp->next;
+    while (tmp->next != nullptr) {
+        cin >> a;
+        if (a == 0) {
+            tmp->next = nullptr;
+            return;
+        }
+        tmp->key = a;
+        tmp->next = new Element;
+        tmp = tmp->next;
+    }
+    delete tmp;
+    return;
+}
+
+void add(Element *&List, int x) {
+    Element* N = new Element;
+    N->key = x;
+    N->next = List;
+    List = N;
+    return;
+}
+
+void delete_list(Element* &List) {
+    while (List != nullptr) {
+        Element* tmp = List;
+        List = tmp->next;
+        delete tmp;
+    }
+    List = nullptr;
+    return;
+}
+
+void add_sort(Element*& List, int a) {
+    Element* tmp = new Element;
+    tmp = List;
+    while (tmp->next != nullptr) {
+        if (List->key > a) {
+            add(List, a);
+            return;
+        }
+        if (tmp->key <= a && tmp->next->key > a) {
+            Element* N = new Element;
+            N->key = a;
+            N->next = tmp->next;
+            tmp->next = N;
+            return;
+        }
+        if (tmp->next->next == nullptr) {
+            Element* N = new Element;
+            N->key = a;
+            N->next = tmp->next;
+            tmp->next = N;
+            return;
+        }
+        tmp = tmp->next;
+    }
+    return;
+}
+
+void delete_key(Element*& List, int k) {
+    Element* tmp = new Element;
+    tmp = List;
+    while (tmp->next != nullptr) {
+        if (tmp->next->key == k) {
+            Element* pt = tmp;
+            tmp->next = tmp->next->next;
+        }
+        tmp = tmp->next;
+    }
+    return;
+}
+
+void data_out(Element* &List) {
+    cout << '\n';
+    Element* tmp = new Element;
+    tmp = List;
+    while (tmp->next != nullptr) {
+        cout << tmp->key << " ";
+        tmp = tmp->next;
+    }
+    return;
+}
+
+void add_to_begin(Element*& List) {
+    int n = 0;
+    data_in(List);
+    cin >> n;
+    add(List, n);
+    data_out(List);
+    delete_list(List);
+}
+
+void add_to_sort(Element*& List) {
+    int n = 0;
+    data_in(List);
+    cin >> n;
+    add_sort(List, n);
+    data_out(List);
+    delete_list(List);
+}
+
+void delete_of_key(Element*& List) {
+    int n = 0;
+    data_in(List);
+    cin >> n;
+    delete_key(List, n);
+    data_out(List);
+    delete_list(List);
+}
+
+int main() {
+    Element* List = nullptr;
+    add_to_begin(List);
+    add_to_sort(List);
+    delete_of_key(List);
+    return 0;
+}
+
