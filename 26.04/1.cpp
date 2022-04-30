2908a379d6794e3327d84076dc0572f6d5aa2832
Bionhen <liubimov.is@phystech.edu>
2022-04-30 12:38:56 +0300

    3

---
+++ 26.04/1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+struct BinaryTree {
+    int data;
+    BinaryTree* left;
+    BinaryTree* right;
+};
+
+void add_tree(BinaryTree*& root, int a) {
+    if (root == nullptr) {
+        root = new BinaryTree;
+        root->data = a;
+        root->left = nullptr;
+        root->right = nullptr;
+        return;
+    }
+    else {
+        if (a > root->data) {
+            add_tree(root->right, a);
+        }
+        else {
+            add_tree(root->left, a);
+        }
+    }
+}
+
+void data_in(BinaryTree* &root) {// если введенный совпадает с уже существующим, то продолжить!
+    int a = 1;
+    while (a != 0) {
+        cin >> a;
+        if (a == 0) {
+            break;
+        }
+        add_tree(root, a);
+    }
+    return;
+}
+
+void data_out(BinaryTree* root) {
+    if (root == nullptr) {
+        return;
+    }
+    cout << "(";
+    data_out(root->left);
+    cout << root->data;
+    data_out(root->right);
+    cout << ")";
+}
+
+void delete_tree(BinaryTree* root) {
+    if (root == nullptr) {
+        delete root;
+        return;
+    }
+    else {
+        delete_tree(root->left);
+        delete_tree(root->right);
+        delete root;
+    }
+}
